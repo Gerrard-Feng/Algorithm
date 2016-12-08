9b3c8596ab82b1a2df961eb96f5ec47197288725
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-12-08 16:30:42 +0800

    commit test program

---
+++ Algorithm/src/com/gerrard/sort/test/TimeTest.java
@@ -0,0 +1,23 @@
+package com.gerrard.sort.test;
+
+import java.util.Random;
+
+import com.gerrard.sort.StraightInsertion;
+
+public class TimeTest {
+
+	public static void main(String[] args) {
+
+		// 模拟数组
+		int[] array = new int[123456];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Random().nextInt(123456) + 1;
+		}
+		
+		// 时间输出：纳秒
+		long s1 = System.nanoTime();
+		StraightInsertion.optimized_3(array);
+		long e1 = System.nanoTime();
+		System.out.println(e1 - s1);
+	}
+}
---
+++ Algorithm/src/com/gerrard/sort/test/VeracityTest.java
@@ -0,0 +1,37 @@
+package com.gerrard.sort.test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import com.gerrard.sort.StraightInsertion;
+
+public class VeracityTest {
+
+	public static void main(String[] args) {
+
+		// 模拟数组
+		int[] array1 = new int[100000];
+		for (int i = 0; i < array1.length; i++) {
+			array1[i] = new Random().nextInt(100000) + 1;
+		}
+
+		// 拷贝数组
+		int[] array2 = Arrays.copyOf(array1, array1.length);
+
+		// 自己实现排序及官方排序
+		StraightInsertion.optimized_3(array1);
+		Arrays.sort(array2);
+
+		// 比对结果
+		boolean flag = true;
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] != array2[i]) {
+				flag = false;
+				break;
+			}
+		}
+
+		// 输出结果
+		System.out.println(flag);
+	}
+}
